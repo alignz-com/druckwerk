456f207e5c55d4fd528ed2282418f33739a3ea92
Pascal Rossi <pascal.rossi@21ilab.com>
2025-10-29 09:48:12 +0000

    bugfix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -5,10 +5,17 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
+const databaseUrl = process.env.DATABASE_URL;
+
 export const prisma =
   global.prisma ??
   new PrismaClient({
     log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    datasources: databaseUrl
+      ? {
+          db: { url: databaseUrl },
+        }
+      : undefined,
   });
 
 if (process.env.NODE_ENV !== "production") {
