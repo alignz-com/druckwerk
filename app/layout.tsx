6370bc41deab9e71bd03a72910301b1f94f6b62c
Pascal Rossi <pascal.rossi@21ilab.com>
2025-11-05 20:09:57 +0000

    added speed insight

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { cookies } from "next/headers";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import SessionProviderWrapper from "./SessionProviderWrapper"; // 👈 import
 import { LocaleProvider } from "@/components/providers/locale-provider";
 import { isLocale } from "@/lib/i18n/messages";
@@ -38,6 +39,7 @@ export default async function RootLayout({
         <SessionProviderWrapper>
           <LocaleProvider initialLocale={locale}>{children}</LocaleProvider>
         </SessionProviderWrapper>
+        <SpeedInsights />
       </body>
     </html>
   );
