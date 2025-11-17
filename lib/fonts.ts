8c4cd9b4f7a0dc195ae5897fe256c5afa0c55a11
Pascal Rossi <pascal@alignz.com>
2025-11-17 10:19:19 +0000

    feat: restore font variables for sans and mono fonts in fonts.ts

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,5 +1,2 @@
-import { GeistMono } from "geist/font/mono";
-import { GeistSans } from "geist/font/sans";
-
-export const fontSans = GeistSans;
-export const fontMono = GeistMono;
+export const fontSans = { variable: "" } as const;
+export const fontMono = { variable: "" } as const;
